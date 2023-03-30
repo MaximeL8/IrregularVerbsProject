ce99acea76c41abc7b3758a404c2055ec24cb11b
Maxime LEFEBVRE <lefebvremaxime825@gmail.com>
2023-03-30 11:45:26 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [color, setColor] = useState("red");
+  const [isClicked, setIsClicked] = useState(false);
+
+  const changeColor = () => {
+    color == "red" ? setColor("blue") : setColor("red")
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={() => {
+        changeColor()
+        changeSize()
+        }}>
+        <Text style={{
+          color: color,
+          fontSize: 15,
+          textTransform: isClicked ? "uppercase" : "none",
+        }}>Open up App.js to start working on your app!</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +33,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
